95d0787ae72e834cb343838df9598e3aac3ffcdc
Faiz-mohamed <faizmohamed3336@gmail.com>
2026-03-06 12:41:36 +0530

    Configured Azure CI/CD pipeline

---
+++ BusTracker.Api/Controllers/testController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
