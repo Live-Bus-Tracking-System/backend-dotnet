19750caacd5a758ebe24f893f20aa7d3ffc37035
Faiz-mohamed <faizmohamed3336@gmail.com>
2026-04-20 14:28:28 +0530

    debug: enable detailed exception stack traces for API testing

--- BusTracker.Api/Program.cs
+++ BusTracker.Api/Program.cs
@@ -108,7 +108,7 @@ namespace BusTracker.Api
             //app.MapOpenApi();
             app.UseSwagger();
             app.UseSwaggerUI();
-            //app.UseDeveloperExceptionPage();
+            app.UseDeveloperExceptionPage();
             //}
 
             // Only redirect to HTTPS in non-development environments.
